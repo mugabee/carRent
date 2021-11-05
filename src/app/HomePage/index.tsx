fd7e1495413c84106e2cb50fef20008b8266a0bc
mugabe richard <mugabe.richard.37@gmail.com>
2021-11-05 23:21:39 +0200

    environment set up

--- src/app/HomePage/index.tsx
+++ src/app/HomePage/index.tsx
@@ -1,2 +1,13 @@
 import React from "react";
 import styled from "styled-components";
+
+const PageContainer = styled.div`
+  ${tw`
+    flex
+    flex-col
+    w-full
+    h-full
+    items-center
+    overflow-x-hidden
+  `}
+`;
